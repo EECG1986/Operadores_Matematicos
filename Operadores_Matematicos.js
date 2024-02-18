5999c31ec2f1b3a5217acf2f8b57646695e48f34
EECG1986 <estivencardona@gmail.com>
2024-02-18 18:17:24 -0500

    Operadores_Matematicos.js

---
+++ Operadores_Matematicos.js
@@ -0,0 +1,21 @@
+let valorA = 15;
+console.log(valorA);
+
+let valorB = 5;
+console.log(valorB);
+
+let suma = valorA + valorB;
+console.log(suma);
+
+let resta = valorA - valorB;
+console.log(resta);
+
+let multiplicacion = valorA * valorB;
+console.log(multiplicacion);
+
+let division = valorA / valorB;
+console.log(division);
+
+let resto = valorA % valorB;
+console.log(resto);
+
